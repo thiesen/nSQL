e6493001a1f42efa336c35db59455280f83cb93d
thiesen <nathanpthiesen@gmail.com>
2018-05-19 19:49:38 +0100

    define basic dsl for meta commands and basic statements

--- db.c
+++ db.c
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum MetaCommandResult_t {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+enum PrepareResult_t {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PrepareResult_t PrepareResult;
+
+enum StatementType_t {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+};
+typedef enum StatementType_t StatementType;
 
 struct InputBuffer_t {
   char *buffer;
@@ -10,6 +27,11 @@ struct InputBuffer_t {
   ssize_t input_length;
 };
 typedef struct InputBuffer_t InputBuffer;
+
+struct Statement_t {
+  StatementType type;
+};
+typedef struct Statement_t Statement;
 
 InputBuffer* new_input_buffer()
 {
@@ -21,8 +43,6 @@ InputBuffer* new_input_buffer()
 
   return input_buffer;
 }
-
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 void print_prompt()
 {
@@ -45,6 +65,45 @@ void read_input(InputBuffer* input_buffer)
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
+{
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+
+    return PREPARE_SUCCESS;
+  }
+  if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+    statement->type = STATEMENT_SELECT;
+
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement)
+{
+  switch (statement->type) {
+  case (STATEMENT_INSERT):
+    printf("This is where we would do an insert.\n");
+
+    break;
+  case (STATEMENT_SELECT):
+    printf("This is where we would do a select.\n");
+
+    break;
+  }
+}
+
 int main(int argc, char* argv[])
 {
   InputBuffer* input_buffer = new_input_buffer();
@@ -53,10 +112,27 @@ int main(int argc, char* argv[])
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+      case (META_COMMAND_SUCCESS):
+        continue;
+      case (META_COMMAND_UNRECOGNIZED_COMMAND):
+        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        continue;
+      }
     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+    case (PREPARE_SUCCESS):
+      break;
+    case (PREPARE_UNRECOGNIZED_STATEMENT):
+      printf("Unrecognized keyword at start of '%s'\n",
+             input_buffer->buffer);
+      continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
